c29ce97d979d98a69af57d2d85ac15ce306e9c08
TeleGold <omid.http@gmail.com>
2016-06-10 02:16:25 +0430

    Update weather.lua

--- plugins/weather.lua
+++ plugins/weather.lua
@@ -12,28 +12,28 @@ local function get_weather(location)
   local weather = json:decode(b)
   local city = weather.name
   local country = weather.sys.country
-  local temp = 'دمای شهر '..city..'\n\n🌡 دمای کنونی هوا : '..weather.main.temp..' C\n\nفشار هوا :'..weather.main.pressure..'\nرطوبت هوا : '..weather.main.humidity..' %\n\n🔻حداقل دمای امروز : '..weather.main.temp_min..'\n🔺حداکثر دمای امروز : '..weather.main.temp_min..'\n\n🌬 سرعت باد : '..weather.wind.speed..'\nدرجه وزش باد : '..weather.wind.deg..'\n\n🔸طول جغرافیایی : '..weather.coord.lon..'\n🔹عرض جغرافیایی : '..weather.coord.lat
-  local conditions = 'شرایط فعلی آب و هوا : '
+  local temp = '🔱 دمای شهر '..city..'\n\n🔱 دمای کنونی هوا : '..weather.main.temp..' C\n\n🔱 فشار هوا :'..weather.main.pressure..'\n🔱 رطوبت هوا : '..weather.main.humidity..' %\n\n🔱 حداقل دمای امروز : '..weather.main.temp_min..'\n🔱 حداکثر دمای امروز : '..weather.main.temp_min..'\n\n🔱  🔱سرعت باد : '..weather.wind.speed..'\n🔱 درجه وزش باد : '..weather.wind.deg..'\n\n🔱 طول جغرافیایی : '..weather.coord.lon..'\n🔱 عرض جغرافیایی : '..weather.coord.lat
+  local conditions = '🔱 شرایط فعلی آب و هوا : '
 
   if weather.weather[1].main == 'Clear' then
-    conditions = conditions .. 'آفتابی ☀'
+    conditions = conditions .. '🔱 آفتابی '
   elseif weather.weather[1].main == 'Clouds' then
-    conditions = conditions .. 'ابری ☁☁'
+    conditions = conditions .. '🔱 ابری '
   elseif weather.weather[1].main == 'Rain' then
-    conditions = conditions .. 'بارانی ☔'
+    conditions = conditions .. '🔱 بارانی '
   elseif weather.weather[1].main == 'Thunderstorm' then
-    conditions = conditions .. 'طوفانی 🌪🌪🌪🌪'
+    conditions = conditions .. '🔱 طوفانی'
   elseif weather.weather[1].main == 'Mist' then
-    conditions = conditions .. 'مه 🌫'
+    conditions = conditions .. '🔱 مه '
   end
 
-  return temp .. '\n\n' .. conditions..'\n\n@WaderTGTeam'
+  return temp .. '\n\n' .. conditions..'\n\n🔱 TeleGold_Team 🔱\n‌'
 end
 local function run(msg, matches) 
     city = matches[1]
   local wtext = get_weather(city)
   if not wtext then
-    wtext = 'مکان وارد شده صحیح نیست'
+    wtext = '🔱 مکان وارد شده صحیح نیست. 🔱'
   end
   return wtext
 end
