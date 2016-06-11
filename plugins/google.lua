e0c96db1dd4e6af88b1c568b67bc284a71039c5c
TeleGold <omid.http@gmail.com>
2016-06-11 07:48:11 +0430

    Update google.lua

--- plugins/google.lua
+++ plugins/google.lua
@@ -34,7 +34,7 @@ return {
 description = "Searches Google and send results",
 usage = "!google [terms]: Searches Google and send results",
 patterns = {
-"^!google (.*)$",
+"^[/#!][gG]oogle (.*)$",
 "^%[/!#][g|G]oogle (.*)$"
 },
 run = run
