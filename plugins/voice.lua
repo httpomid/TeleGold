90f77fa0990f626a1b12ba354897d9134b5d0596
TeleGold <omid.http@gmail.com>
2016-06-11 09:38:06 +0430

    Update voice.lua

--- plugins/voice.lua
+++ plugins/voice.lua
@@ -1,18 +1,34 @@
 do
+
 local function run(msg, matches)
-  local url = "http://www.farsireader.com/PlayText.aspx?text="..matches[1]
+
+
+local text = matches[1]
+
+  local b = 1
+
+  while b ~= 0 do
+    textc = text:trim()
+    text,b = text:gsub(' ','.')
+    
+    
+  if msg.to.type == 'user' then 
+      return nil
+      else
+  local url = "http://tts.baidu.com/text2audio?lan=en&ie=UTF-8&text="..textc
   local receiver = get_receiver(msg)
-  local file = download_to_file(url,'text.ogg')
+  local file = download_to_file(url,'Hextor.ogg')
       send_audio('channel#id'..msg.to.id, file, ok_cb , false)
 end
-
+end
+  end
 return {
   description = "text to voice",
   usage = {
     "!voice [text]"
   },
   patterns = {
-    "^[!/#]voice (.+)$"
+    "^[!/#][vV]oice +(.*)$",
   },
   run = run
 }
