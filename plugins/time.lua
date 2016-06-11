5a76c2a716b70428e8da44b76063845bbfed69dd
TeleGold <omid.http@gmail.com>
2016-06-11 08:09:30 +0430

    Update time.lua

--- plugins/time.lua
+++ plugins/time.lua
@@ -1,20 +1,11 @@
-
-
-function run(msg, matches)
+#addplug function run(msg, matches)
 local url , res = http.request('http://api.gpmod.ir/time/')
-if res ~= 200 then return "🔱 ارتباط ما با سرور قطع شد. 🔱" end
+if res ~= 200 then return "No connection" end
 local jdat = json:decode(url)
-local text = '🔱 ساعت '..jdat.FAtime..' \n🔱 امروز '..jdat.FAdate..' میباشد.\n    ----\n🔱 '..jdat.ENtime..'\n🔱 '..jdat.ENdate.. '\n🔱 @TeleGold_Team 🔱\n‌‌‌'
+local text = '🔱 ساعت '..jdat.FAtime..' \n🔱 امروز '..jdat.FAdate..' میباشد.\n    ----\n🔱 '..jdat.ENtime..'\n🔱 '..jdat.ENdate.. '\n🔱 @TeleGold_Team 🔱'
 return text
 end
 return {
-  patterns = {
-  "^[/!]([Tt][iI][Mm][Ee])$"
- "^(زمان)$",
-  "^(ساعت)$",
-   "^(تاریخ)$",
-   "^(ساعت و تاریخ)$"
-  }, 
-run = run 
-}
-
+  patterns = {"^[/!]([Tt][iI][Mm][Ee])$","^زمان$"}, 
+run = run
+} time
