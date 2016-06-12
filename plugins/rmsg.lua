5eb9557abf6d02c4b4275c2f0a2f3303fe00d68e
TeleGold <omid.http@gmail.com>
2016-06-12 10:19:04 +0430

    Update rmsg.lua

--- plugins/rmsg.lua
+++ plugins/rmsg.lua
@@ -3,28 +3,29 @@ local function history(extra, suc, result)
     delete_msg(result[i].id, ok_cb, false)
   end
   if tonumber(extra.con) == #result then
-    send_msg(extra.chatid, '"'..#result..'"🔱  پیام اخیر سوپر گروه حذف شد. 🔱', ok_cb, false)
+    send_msg(extra.chatid, '"'..#result..'"🔱 پیام اخیر سوپر گروه حذف شد', ok_cb, false)
   else
-    send_msg(extra.chatid, '🔱 ️تعداد پیام مورد نظر شما پاک شد. 🔱', ok_cb, false)
+    send_msg(extra.chatid, '🔱تعداد پیام مورد نظر شما پاک شد🔱', ok_cb, false)
   end
 end
 local function run(msg, matches)
   if matches[1] == 'rmsg' and is_owner(msg) then
     if msg.to.type == 'channel' then
       if tonumber(matches[2]) > 10000 or tonumber(matches[2]) < 1 then
-        return "🔱 تعداد بیشتر از 1 مجاز است. 🔱"
-      endhy      get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
+        return "🔱تعداد بیشتر از 1 مجاز است🔱"
+      end
+      get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
     else
-      return "🔱 ️فقط در سوپرگروه ممکن است. 🔱"
+      return "🔱فقط در سوپرگروه ممکن است🔱"
     end
   else
-    return "🔱 ️شما دسترسی ندارید. 🔱"
+    return "🔱شما دسترسی ندارید🔱"
   end
 end
 
 return {
     patterns = {
-        '^[/#][rR]msg (%d*)$'
+        '^[!/#](rmsg) (%d*)$'
     },
     run = run
 }
