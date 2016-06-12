a8ff36ef88c1470309dac3b0cf169d69ed82334f
TeleGold <omid.http@gmail.com>
2016-06-12 08:09:59 +0430

    Update fal.lua

--- plugins/fal.lua
+++ plugins/fal.lua
@@ -1,12 +1,15 @@
-local function run(msg, matches)
-  local htp = http.request('http://emrani.net/Hafez/api/hafez/fal')
-  local data = json:decode(htp)
-  print(data)
-  return data.poem
+local database = 'http://vip.opload.ir/vipdl/95/1/amirhmz/'
+local function run(msg)
+	local res = http.request(database.."fal.db")
+	local fal = res:split(",") 
+	return fal[math.random(#fal)]
 end
 return {
-  patterns = {
-    "^[!/#]fal$"
-  },
-  run = run
+	description = "500 Fal Hafez",
+	usage = "!joke : send random fal",
+	patterns = {
+		"^[/#!][fF]al",
+		"^(فال بگیر)$"
+		},
+	run = run
 }
