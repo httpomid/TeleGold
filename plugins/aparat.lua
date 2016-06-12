5454c7dfc82d56d8a4b56509fc193971b510cf5a
TeleGold <omid.http@gmail.com>
2016-06-12 08:26:01 +0430

    Update aparat.lua

--- plugins/aparat.lua
+++ plugins/aparat.lua
@@ -1,23 +1,23 @@
---@TeleGold_Team
+
 
 local function run(msg, matches)
- if matches[1]:lower() == 'aparat' then
-  local url = http.request('http://www.aparat.com/etc/api/videoBySearch/text/'..URL.escape(matches[2]))
-  local jdat = json:decode(url)
+	if matches[1]:lower() == 'aparat' then
+		local url = http.request('http://www.aparat.com/etc/api/videoBySearch/text/'..URL.escape(matches[2]))
+		local jdat = json:decode(url)
 
-  local items = jdat.videobysearch
-  text = '🔱 نتیجه‌ی جستجو در آپارات:\n'
-  for i = 1, #items do
-  text = text..'\n'..i..'- '..items[i].title..'  -  🔱 تعداد بازدید فیلم: '..items[i].visit_cnt..'\n   🔱 لینک برای نمایش: aparat.com/v/'..items[i].uid
-  end
-  text = text..'\n\n🔱 @TeleGold_Team 🔱'
-  return text
- end
+		local items = jdat.videobysearch
+		text = '🔱 نتیجه جستجو در آپارات: \n'
+		for i = 1, #items do
+		text = text..'\n'..i..'- '..items[i].title..'  -  تعداد بازدید: '..items[i].visit_cnt..'\n    لینک: aparat.com/v/'..items[i].uid
+		end
+		text = text..'\n\n🔱 @TeleGold_Team 🔱'
+		return text
+	end
 end
 
 return {
    patterns = {
-"^[#/!][aA]parat (.*)$",
+"^[/#!][aA]parat (.*)$",
    },
    run = run
 }
