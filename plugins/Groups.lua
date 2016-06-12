72f919eb948eef13244e49fe28d28b88a82ab56a
TeleGold <omid.http@gmail.com>
2016-06-12 08:45:38 +0430

    Update Groups.lua

--- plugins/Groups.lua
+++ plugins/Groups.lua
@@ -2,9 +2,9 @@ local function chat_list(msg)
     local data = load_data(_config.moderation.data)
         local groups = 'groups'
         if not data[tostring(groups)] then
-                return 'No groups at the moment'
+                return '🔱 گروهی یافت نشد! 🔱'
         end
-        local message = 'List Groups WaderTG:\n*Use #join [ID-CHAT] to join\n*Use #link [ID-CHAT] send link chat\n\n '
+        local message = '🔱 لیست گروهای ربات تله گولد:\n🔱*برای جوین دادن اینگونه عمل کنید: #join [آیدی گروه]\n🔱*و برای ارسال لینک گروه به این شکل: #link [آیدی گروه] \n\n '
         for k,v in pairs(data[tostring(groups)]) do
                 local settings = data[tostring(v)]['settings']
                 for m,n in pairsByKeys(settings) do
@@ -27,28 +27,28 @@ local function run(msg, matches)
   if is_sudo(msg) then
     if matches[1] == 'link' and data[tostring(matches[2])] then
         if is_banned(msg.from.id, matches[2]) then
-     return 'You are in ban'
+     return '🔱 شما از گروه مسدود (بن) شدید. 🔱'
   end
       if is_gbanned(msg.from.id) then
-            return 'You are in Globalban or Superban'
+            return '🔱 متأسفیم، شما بصورت دائم از گروهای ربات تله گولد محروم شدید، اگر احساس میکنید اشکتباهی رخ داده است به کانال @TeleGold_Team بروید و با سازندگان در ارتباط شوید. 🔱'
       end
       if data[tostring(matches[2])]['settings']['lock_member'] == 'yes' and not is_owner2(msg.from.id, matches[2]) then
-        return 'Group is private.'
+        return '🔱 متأسفم، این گروه شخصی است و اجازه ورود را ندارید! 🔱'
       end
           local chat_id = "chat#id"..matches[2]
           local user_id = "user#id"..msg.from.id
       chat_add_user(chat_id, user_id, ok_cb, false)   
    local group_link = data[tostring(matches[2])]['settings']['set_link']
    local group_name = data[tostring(matches[2])]['settings']['set_name']
-   return "Group Link:\n"..group_link.."\n\n(Group name:"..group_name..")\nCreate by:@WaderTGTeam"
+   return "🔱 بفرمایید پیشاپیش خوش آمدید:\n"..group_link.."\n\n🔱(نام گروه:"..group_name..")🔱"
    
     elseif matches[1] == 'link' and not data[tostring(matches[2])] then
 
-          return "Group not found"
+          return "🔱 گروه یافت نشد! 🔱"
          end
     end
   else
-   return "Just for sudo!"
+   return "🔱 فقط برای سازنده اصلی امکان پذیر است! 🔱"
   end
 
      if matches[1] == 'groups' then
