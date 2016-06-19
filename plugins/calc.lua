1afb8deda54febabbf287959710ae80ee91b2e1c
TeleGold <omid.http@gmail.com>
2016-06-19 19:10:15 +0430

    Create calc.lua

---
+++ plugins/calc.lua
@@ -0,0 +1,23 @@
+local function mathjs(exp)
+  local url = 'http://api.mathjs.org/v1/'
+  url = url..'?expr='..URL.escape(exp)
+  local b,c = http.request(url)
+  local text = nil
+  if c == 200 then
+    text = '🔱> '..b
+  
+  elseif c == 400 then
+    text = b
+  else
+    text = 'Unexpected error\n'
+      ..'Is api.mathjs.org up?'
+  end
+  return text end local function run(msg, matches)
+  return mathjs(matches[1]) end return {
+  description = "Calculate math expressions with mathjs API",
+  usage = "!calc [expression]: evaluates the expression and sends the result.",
+  patterns = {
+    "^[!/#][cC]alc (.*)$"
+  },
+  run = run
+}
