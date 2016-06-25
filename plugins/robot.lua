0cd5518748159d471a934f3737c2fb6798bc23af
TeleGold <omid.http@gmail.com>
2016-06-25 18:19:17 +0430

    Update robot.lua

--- plugins/robot.lua
+++ plugins/robot.lua
@@ -1,32 +1,26 @@
-#addplug function run(msg, matches)
+function run(msg, matches)
 local reply = msg['id']
-local text = 'جان :) '
-local text2 = 'جونم بابایی 😍'
-local text3 = 'جانم مدیر :)'
-local text4 = 'جان ادمین جون :)'
+local text = 'جان :)'
+local text2 = 'جونم بابایی خسته نباشی😍'
 if not is_sudo(msg) then
 reply_msg(reply, text, ok_cb, false)
 elseif is_sudo(msg) then
 reply_msg(reply, text2, ok_cb, false)
-elseif is_owner(msg) and not is_sudo(msg) then
-reply_msg(reply, text3, ok_cb, false)
-elseif is_momod(msg) then
-reply_msg(reply, text4, ok_cb, false)
 end
 end
 return {
 patterns = {
-"^ربوت$",
-"^[rR]obot$",
-"^[bB]ot$",
-"^[rR]obot?$",
-"^[rR]obot??$",
-"^[bB]ot?$",
-"^ربات$",
+"^تله گولد$",
+"^تله گولد؟$",
+"^رباتی؟$",
+"^ربات جون$",
+"^ربات جون؟$",
+"^ربوت؟$",
 "^ربات؟$",
-"^ربات؟؟$",
-"^ربات😍$",
-"^ربات جون$"
+"^ربوت؟$",
+"^ربات$",
+"^رباط؟$",
+"^رباط$"
 },
 run = run
-} robot
+}
