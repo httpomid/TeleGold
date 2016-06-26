bd80a4b26a5a45ddc761740e374899f20982109d
TeleGold <omid.http@gmail.com>
2016-06-26 23:12:35 +0430

    Update delplug.lua

--- plugins/delplug.lua
+++ plugins/delplug.lua
@@ -1,15 +1,12 @@
-
 local function run(msg, matches)
     if matches[1] == "delplug" and is_sudo(msg) then
 text = io.popen("cd plugins && rm "..matches[2]..".lua")
-return text.."🔱پلاگینی با نام: ["..matches[2].."] با موفقیت حذف شد🔱"
+return text.."["..matches[2].."]حذف شد"
 end 
 end
-
 return { 
 patterns = {
- 
-'^[!#/](delplug) (.*)$' 
+'^([dD]elplug) (.*)$' 
 },
 run = run,
 }
