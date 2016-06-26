ebf70e7a4b5c4cbecb1f668d74ddb5d181658065
TeleGold <omid.http@gmail.com>
2016-06-26 18:41:42 +0430

    Update cap.lua

--- plugins/cap.lua
+++ plugins/cap.lua
@@ -1,9 +1,7 @@
 do
 local function action_by_reply22(extra, success, result)
 if result.media then
-
-
-return  send_msg(extra.receiver, '🔱 متن زیر عکس 🔱\n\n___________________\n'.. result.media.caption, ok_cb, true)
+return  send_msg(extra.receiver, ''.. result.media.caption, ok_cb, true)
 end
 if result.service then
 return  send_msg(extra.receiver, result.service.type, ok_cb, true)
@@ -11,25 +9,17 @@ end
 end
 local function run(msg, matches)
 if msg.to.type == 'chat' or msg.to.type == 'channel' then
-
 local receiver = get_receiver(msg) 
    if msg.reply_id then
-
   msgr = get_message(msg.reply_id, action_by_reply22, {receiver=receiver})
 return 
-
 end
 end
 end
-
-
 return {
   description = "Simplest plugin ever!",
   usage = "!echo [whatever]: echoes the msg",
-  patterns = {
-    "^[!#/]cap$",
-
-  }, 
+  patterns = {"^[cC]ap$",}, 
   run = run
 }
 end
