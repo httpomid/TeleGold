1ce03b690b1432acb2280dafd4e61c6b343f98de
TeleGold <omid.http@gmail.com>
2016-06-26 19:35:15 +0430

    Update wai.lua

--- plugins/wai.lua
+++ plugins/wai.lua
@@ -1,25 +1,19 @@
 do
-
 local function run(msg, matches)
   if matches[1] == 'wai' then
     if is_sudo(msg) then
-     --send_document(get_receiver(msg), "/home/Seed/axs/sudo.webp", ok_cb, false)
-      return "🔱 با عرض ادب شما سازنده ربات تله گلد هستید! 🔱"
+      return "شما سازنده ربات تله گلد هستید"
     elseif is_owner(msg) then
-   -- send_document(get_receiver(msg), "/home/Seed/axs/owner.webp", ok_cb, false)
-      return "🔱 شما مدیر گروه هستید! 🔱"
+      return " شما مدیر گروه هستید! "
     elseif is_momod(msg) then
-  --  send_document(get_receiver(msg), "/home/Seed/axs/mod.webp", ok_cb, false)
-      return "🔱 شما دستیار مدیر گروه (ادمین) هستید! 🔱"
+      return "شما دستیار مدیر گروه (ادمین) هستید!"
     else
-  --  send_document(get_receiver(msg), "/root/Tele/axs/mmbr.webp", ok_cb, false)
-      return "🔱 شما جایگاهی خاصی ندارید! 🔱"
+      return "شما جایگاهی خاصی ندارید!"
     end
   end
 end
-
 return {
-  patterns = {"^[#!/]([Ww]ai)$",},
+  patterns = {"^([Ww]ai)$",},
   run = run
 }
 end 
