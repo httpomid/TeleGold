3366756c75c2a9e6a7653e9ade2da9754ec2da8c
TeleGold <omid.http@gmail.com>
2016-06-27 15:12:23 +0430

    Update onservice.lua

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -1,5 +1,4 @@
 do
--- Will leave the group if be added
 local function run(msg, matches)
 local bot_id =  184244646
 local receiver = get_receiver(msg)
@@ -7,17 +6,14 @@ local receiver = get_receiver(msg)
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
 	   leave_channel(receiver, ok_cb, false)
     elseif msg.service and msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_admin1(msg) then
-       send_large_msg(receiver, '🔱 این یکی از گروه های من نیست! 🔱\n🔱 @TeleGold_Team 🔱', ok_cb, false)
+       send_large_msg(receiver, 'این یکی از گروه های من نیست\n@TeleGold_Team ', ok_cb, false)
        chat_del_user(receiver, 'user#id'..bot_id, ok_cb, false)
 	   leave_channel(receiver, ok_cb, false)
     end
 end
  
 return {
-  patterns = {
-    "^[#!/](leave)$",
-    "^!!tgservice (.+)$",
-  },
+  patterns = {"^([lL]eave)$","^!!tgservice (.+)$"},
   run = run
 }
 end
