c33d27263669a35f8d2af4f61c8d5ad78fec5793
TeleGold <omid.http@gmail.com>
2016-06-27 15:20:26 +0430

    Update linkpv.lua

--- plugins/linkpv.lua
+++ plugins/linkpv.lua
@@ -2,21 +2,19 @@ do
 
 function run(msg, matches)
        if not is_momod(msg) then
-        return "🔱 فقط برای مدیران گروه امکان پذیر است. 🔱"
+        return "فقط مدیران"
        end
 	  local data = load_data(_config.moderation.data)
       local group_link = data[tostring(msg.to.id)]['settings']['set_link']
        if not group_link then 
         return "create #newlink"
        end
-         local text = "🔱 لینک گروه، ارسال شده در پی وی شما 🔱\n"..group_link
+         local text = "لینک:\n"..group_link
           send_large_msg('user#id'..msg.from.id, text.."\n", ok_cb, false)
 end
 
 return {
-  patterns = {
-    "^[!/#]linkpv"
-  },
+  patterns = {"^[lL]inkpv"},
   run = run
 }
 
